9f37696e03ab32835bfab0f26548cfa2437a95d0
gayshub <jl@zhangxianglei.com>
2017-08-31 11:57:02 +0800

    Create something.vue

---
+++ something.vue
@@ -0,0 +1,19 @@
+<template>
+  .....
+  
+  <nav class="pagination is-right " role="navigation" aria-label="pagination" style="padding-right: 15px">
+			<ul class="pagination-list" >
+				<div>
+					<li><a class="pagination-link" aria-label="Goto page 1" @click="getLinodeData(1)"><</a></li>
+				</div>
+				<div v-for="(n,index) in totalPages">
+					<li><a class="pagination-link" :class="{ 'is-current': index+1 === currentPage }" @click="getLinodeData(index+1)">{{ n }}</a></li>
+				</div>
+				<div>
+					<li><a class="pagination-link" aria-label="Goto " @click="getLinodeData(totalPages)">></a></li>
+				</div>
+			</ul>
+		</nav>
+    
+    ....
+ </template>
